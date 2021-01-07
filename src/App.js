2a2d1c500d32eda8528d0bb729cca61d3893e91b
David Portilla <dportilla@adkgroup.com>
2021-01-07 13:11:31 -0500

    adding initial styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from '@emotion/styled'
 
-function App() {
+const Wrapper = styled.div`
+ display: flex;
+ align-items: center;
+ padding-top: 5rem;
+ flex-direction: column-reverse;
+`
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0% ,#007d35 40%, #07574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  padding: 1rem 3rem;
+  font-size:2rem;
+  border: 2px solid black;
+`
+
+function App () {
+
+  const fetchAPI = () => {
+    console.log('getting data...')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Button
+        onClick={fetchAPI}
+      > Get a Quote</Button>
+    </Wrapper>
   );
 }
 
